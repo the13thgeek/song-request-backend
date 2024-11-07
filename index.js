e7e147ab8c4dc6f6ca1308157953f15b32812ac0
the13thgeek <aldreich.aguilar@yahoo.com>
2024-11-06 20:45:21 -0500

    Added DDR 3rdMIX Korea2 + DDR 4thMIX Plus libraries

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 1300;
 
 // Define song library
-const songs = require('./data/ddr3mk.json');
+const songs = require('./data/ddr4mp.json');
 
 function findSong(query) {
     const sanitizedQuery = query.toLowerCase();
@@ -34,26 +34,13 @@ app.post('/request-song', (req, res) => {
     let requestedSong = findSong(req.query.songtitle);
     if(requestedSong) {
         broadcast({ type: "ADD_SONG", song: { title: requestedSong.title, artist: requestedSong.artist, user: req.query.user } });
-        res.status(200).send('Request has been added: ' + requestedSong.title + '/' + requestedSong.artist);
-        console.log('Request has been added: ' + requestedSong.title + '/' + requestedSong.artist);
+        res.status(200).send('Request has been added: [' + requestedSong.title + '/' + requestedSong.artist + ']');
+        console.log('Request has been added: [' + requestedSong.title + '/' + requestedSong.artist + ']');
     } else {
         res.status(200).send("Sorry, no songs matched \"" + req.query.songtitle + ".\"");
         console.log("No songs matched \"" + req.query.songtitle + ".\"");
     }
 });
-
-// app.post('/add-song', (req, res) => {
-//     const { title, artist, user } = req.body;
-    
-//     if(title && artist && user) {
-//         broadcast({ type: 'ADD_SONG', song: { title, artist, user } });
-//         res.status(200).send('Request added');
-//         console.log('Request added');
-//     } else {
-//         res.status(400).send("Error");
-//         console.log('Error');
-//     }
-// });
 
 app.post('/remove-song', (req, res) => {
     broadcast({ type: 'REMOVE_SONG' });
