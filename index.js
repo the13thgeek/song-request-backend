90e0edec057c5e2b83588c4ec3b4708324747db4
the13thgeek <aldreich.aguilar@yahoo.com>
2024-11-08 18:02:32 -0500

    Updated logic, updated corrected X3 database

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = 1300;
 
 // Define song library
 const songs = require('./data/ddr3mk.json');
+let queue = [];
 
 function findSong(query) {
     console.log('Searching for "' + query.trim() + '"');
@@ -16,7 +17,7 @@ function findSong(query) {
         const romanizedTitle = song?.romanizedTitle?.toLowerCase() || "";
         const romanizedArtist = song?.romanizedArtist?.toLowerCase() || "";
 
-        console.log("Searching: " + song.title)
+        //console.log("Searching: " + song.title)
         
         return words.every(word =>
             title.includes(word) ||
@@ -48,18 +49,31 @@ app.post('/request-song', (req, res) => {
     let requestedSong = findSong(req.query.songtitle);
     if(requestedSong) {
         broadcast({ type: "ADD_SONG", song: { title: requestedSong.title, artist: requestedSong.artist, user: req.query.user } });
-        res.status(200).send('Request has been added: [' + requestedSong.title + '/' + requestedSong.artist + ']');
-        console.log('Request has been added: [' + requestedSong.title + '/' + requestedSong.artist + ']');
+        queue.push( { title: requestedSong.title, artist: requestedSong.artist, user: req.query.user } );
+        res.status(200).send(`✔️ Request has been added: [${requestedSong.title} / ${requestedSong.artist}]`);
+        console.log(`Request has been added: [${requestedSong.title} / ${requestedSong.artist}]`);
     } else {
-        res.status(200).send("Sorry, no songs matched \"" + req.query.songtitle + ".\"");
+        res.status(200).send(`❌ Sorry, no songs matched "${req.query.songtitle}." The song you requested may not be in the current game.`);
         console.log("No songs matched \"" + req.query.songtitle + ".\"");
     }
 });
 
 app.post('/remove-song', (req, res) => {
+    
+    if(queue.length === 0) { console.log('No songs in queue to remove.'); return; }
+    
     broadcast({ type: 'REMOVE_SONG' });
-    res.status(200).send("Request has been fulfilled.");
-    console.log("Request has been fulfilled.")
+    let currSong = queue[0];
+    queue = queue.slice(1);
+    if(queue.length > 0) {
+        res.status(200).send(`▶️ Request [${currSong.title}] has been played! Next song ⏩ [${queue[0].title}]`);
+        console.log(`Request [${currSong.title}] has been played! Next song ⏩ [${queue[0].title}]`);
+    } else {
+        res.status(200).send(`▶️ Request [${currSong.title}] has been played. There are no songs in queue.`);
+        console.log(`Request [${currSong.title}] has been played. There are no songs in queue.`)
+    }
+    
+    
 });
 
 // HTTP->Websocket
